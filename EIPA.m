1d9d48929c6ea7e131ef0b4a4ec7fafd9e5cd2a1
Adam Heffernan <adamheffernan@live.com>
2020-02-07 14:11:53 -0500

    Completition of PA5

--- EIPA.m
+++ EIPA.m
@@ -1,68 +1,66 @@
-global C;
-C.q_0 = 1.60217653e-19;             % electron charge
-C.hb = 1.054571596e-34;             % Dirac constant
-C.h = C.hb * 2 * pi;                    % Planck constant
-C.m_0 = 9.10938215e-31;             % electron mass
-C.kb = 1.3806504e-23;               % Boltzmann constant
-C.eps_0 = 8.854187817e-12;          % vacuum permittivity
-C.mu_0 = 1.2566370614e-6;           % vacuum permeability
-C.c = 299792458; % speed of light
+%Adam Heffernan 100977570 2/7/2020
 
-nx = 75;
+q_0 = 1.60217653e-19;             % electron charge
+hb = 1.054571596e-34;             % Dirac constant
+h = C.hb * 2 * pi;                    % Planck constant
+m_0 = 9.10938215e-31;             % electron mass
+kb = 1.3806504e-23;               % Boltzmann constant
+eps_0 = 8.854187817e-12;          % vacuum permittivity
+mu_0 = 1.2566370614e-6;           % vacuum permeability
+c = 299792458; % speed of light
+
+nx = 50;
 ny = 50;
 
-dx = 1;
-dy = 1;
 G = sparse(nx*ny, ny*nx);
 alpha = (C.hb^2) / (2 * C.m_0);
 
-map = @(i,j) j + (i - 1)*ny;
+mapping_equation = @(i,j) j + (i - 1)*ny;
 
 
 for i=1:nx
     for j=1:ny
-        n = map(i,j);
-        nxm = map(i-1,j);
-        nxp = map(i+1,j);
-        nym = map(i,j-1);
-        nyp = map(i,j+1);
+        current_cell = mapping_equation(i,j);
+        nx_left = mapping_equation(i-1,j);
+        nx_right = mapping_equation(i+1,j);
+        ny_bottom = mapping_equation(i,j-1);
+        ny_top = mapping_equation(i,j+1);
         
         if (i == 1 || i == nx)
-            G(n,n) = 1 / dx^2;
+            G(current_cell,current_cell) = 1; 
         elseif (j == 1 || j == ny)
-            G(n,n) = 1 / dy^2;
+            G(current_cell,current_cell) = 1;
             
-        elseif (i > 10 & i <20 & j > 10 & j <20)
-            G(n,n) = -1 / dx^2 + -1 / dy^2;
-            G(n,nxm) = 1 / dx^2;
-            G(n,nxp) = 1 / dx^2;
-            G(n,nym) = 1 / dy^2;
-            G(n,nyp) = 1 / dy^2;
+        elseif (i > 10 && i < 20 && j > 10 && j < 20)
+            
+            G(current_cell,current_cell) = -2;
         else
-            G(n,n) = -2 / dx^2 + -2 / dy^2;
-            G(n,nxm) = 1 / dx^2;
-            G(n,nxp) = 1 / dx^2;
-            G(n,nym) = 1 / dy^2;
-            G(n,nyp) = 1 / dy^2;
+            G(current_cell,current_cell) = -4;
+            G(current_cell,nx_left) = 1;
+            G(current_cell,nx_right) = 1; 
+            G(current_cell,ny_bottom) = 1; 
+            G(current_cell,ny_top) = 1;
         end
     end
 end
-
-%spy(G);
-[E,D] = eigs(G,9,'SM');
-
+figure(2);
+spy(G);
 E_plot = zeros(nx,ny,9);
-for n=1:9
+
+figure(3);
+[E,D] = eigs(G,9,'SM');
+plot(D);
+figure(1)
+hold on;
+
+for current_cell=1:9
     for i=1:nx
         for j=1:ny
-            E_plot(i,j,n) = E(map(i,j),n);
+            E_plot(i,j,current_cell) = E(mapping_equation(i,j),current_cell);
         end
     end
+    subplot(3,3,current_cell);
+    surf(E_plot(:,:,current_cell));
 end
 
-f = figure();
-hold on;
-for n=1:9
-    subplot(3,3,n);
-    surf(E_plot(:,:,n),'EdgeColor','none','LineStyle','none','FaceLighting','phong');
-end
+
